6b0f83eea93ee3e79fc7b71cddee34049bd4a975
Erik Villavera <villaveraerik@gmail.com>
2021-02-25 17:15:44 -0500

    FIx more stuff

---
+++ src/components/CommentForm.js
@@ -0,0 +1,12 @@
+function CommentForm() {
+    return (
+        <form>
+            <div>
+                <label>Adding a comment?</label>
+                <input placeholder='Write your comment here!'/>
+            </div>
+        </form>
+    )
+}
+
+export default CommentForm
